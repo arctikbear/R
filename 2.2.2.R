858ba78b1febbfd6fc678e2cb03c30f777a08777
arctikbear <sk1alexey@yandex.ru>
2017-05-13 22:34:01 +0300

    first commit

---
+++ 2.2.2.R
@@ -0,0 +1,23 @@
+#edited for git
+
+df <- data.frame(x = 1:4, y = LETTERS[1:4], z = c(T, F),
+                 row.names = c("Alpha", "Bravo",
+                               "Charlie", "Delta"))
+
+
+#2.2.5
+#step 1
+sub_attitude <- subset(attitude,learning >
+                         sort(attitude$learning)
+                       [length(attitude$learning) - 5],
+                  c("complaints", "raises", "advance"))
+#step 2
+names(which.max(rowSums(subset(attitude,learning >
+         sort(attitude$learning)
+       [length(attitude$learning) - 5],
+       c("complaints", "raises", "advance")))))
+
+#other solution in 1 line
+names(which.max(rowSums(attitude[order(
+  -attitude$learning),][1:5,][c("complaints",
+                                "raises","advance")])))
